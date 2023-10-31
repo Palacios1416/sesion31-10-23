3526fbbd8ea0cf13f8655955955dad6e53e1e2ac
Palacios1416 <130609254+Palacios1416@users.noreply.github.com>
2023-10-31 17:27:42 -0600

    Add files via upload

--- IntroPOO/src/App.java
+++ IntroPOO/src/App.java
@@ -2,8 +2,16 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner scan = new Scanner(System.in);
-        s
-        System.out.println("Hello, World!");
-
+        Pelicula movie = new Pelicula();
+        movie.setNombre("Avengers Endgame");
+        movie.setDuracion(110);
+        movie.setGenero("Accion");
+        movie.setCalidad("4k");
+        movie.setClasificacion("PG13");
+        System.out.println("Nombre: " + movie.getNombre());
+        System.out.println("Duracion: "+movie.getDuracion()+ " minutos.");
+        System.out.println("Genero: "+movie.getGenero());
+        System.out.println("Calidad: "+movie.getCalidad());
+        System.out.println("Clasificacion: "+movie.getClasificacion());
     }
 }
--- IntroPOO/src/Pelicula.java
+++ IntroPOO/src/Pelicula.java
@@ -21,8 +21,39 @@ public class Pelicula {
     public String getNombre(){
         return this.nombre;
     }
-    //mientras que los metodos setters son para modificar
+    public int getDuracion(){
+        return this.duracion;
+    }
+    public String getGenero(){
+        return this.genero;
+    }
+    public String getCalidad(){
+        return this.calidad;
+    }
+    public String getClasificacion(){
+        return this.clasificacion;
+    }
+    //mientras que los me todos setters son para modificar
     public void setNombre(String nombre){
         this.nombre = nombre;
+    }
+    public void setDuracion(int duracion){
+        if (duracion < 0) {
+            this.duracion=0;
+        } else if(duracion > 1 && duracion < 60) {
+            this.duracion=duracion;
+        }else if(duracion > 61) {
+            this.duracion=duracion;
+        }
+        
+    }
+    public void setGenero(String genero){
+        this.genero=genero;
+    }
+    public void setCalidad(String calidad){
+        this.calidad = calidad;
+    }
+    public void setClasificacion(String clasificacion){
+        this.clasificacion=clasificacion;
     }
 }
